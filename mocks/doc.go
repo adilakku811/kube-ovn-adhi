06b691db800a9437261d88aad395c4525618f0ed
adilakku811 <adilakku72@gmail.com>
2025-07-20 01:03:48 +0530

    Update doc.go

--- mocks/doc.go
+++ mocks/doc.go
@@ -6,3 +6,4 @@ Package mocks will have all the mocks
 
 // OvnClient mocks
 //go:generate go tool go.uber.org/mock/mockgen -source=../pkg/ovs/interface.go -destination=./pkg/ovs/interface.go -package=ovs
+ovn clin go tool go uber 
